f99242308c4c71bfb64d274a6d495fe94ec53697
Gerardo <gera_lps@hotmail.com.ar>
2021-03-25 12:24:06 -0300

    Webserver listo

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+require('dotenv').config();
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
-const port = 8080; 
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 // Servir contenido estático
 app.use( express.static('public') );
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hola mundo en su respectiva ruta')
+// app.get('/hola-mundo', (req, res) => {
+//   // res.send('Hola mundo en su respectiva ruta')
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
+app.get('/', (req, res) => {
+  // res.send('Hola mundo en su respectiva ruta')
+  res.render('home', {
+    nombre: 'Gerardo Estrada',
+    titulo: 'Curso de Node'
+  });
+});
+
+app.get('/generic', (req, res) => {
+  // res.send('Hola mundo en su respectiva ruta')
+  res.render('generic', {
+    nombre: 'Gerardo Estrada',
+    titulo: 'Curso de Node'
+  });
+});
+
+app.get('/elements', (req, res) => {
+  // res.send('Hola mundo en su respectiva ruta')
+  res.render('elements', {
+    nombre: 'Gerardo Estrada',
+    titulo: 'Curso de Node'
+  });
 });
 
 app.get('*', (req, res) => {
